79813f13a40ae3fbf6bb860f828c7c0a25f13244
Rushi <deshmukhrushikesh51@gmail.com>
2025-07-05 13:44:28 +0530

    Assignment 1 On Variables, Data Types

---
+++ Assignment 1 On Variables, Data Types/Q3. Addition of 2 Numbers Given by User.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+
+    int No1 = 0, No2 = 0, Add = 0;
+
+    printf("Enter 2 Numbers: \n");
+    scanf("%d%d",&No1,&No2);
+
+    printf("Addition of Given Numbers is %d + %d = %d",No1,No2,No1+No2);
+
+    getch();
+    return 0;
+
+}
