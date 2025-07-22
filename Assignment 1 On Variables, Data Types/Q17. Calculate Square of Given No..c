33b16486b1ff31a5369d65349b7b9a71660f0669
Rushi <deshmukhrushikesh51@gmail.com>
2025-07-22 13:46:39 +0530

    Add files via upload

---
+++ Assignment 1 On Variables, Data Types/Q17. Calculate Square of Given No..c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int n = 0;
+    printf("Enter No : ");
+    scanf("%d", &n);
+
+    printf("The Square of Given Number is %d", n*n);
+
+    getch();
+    return 0;
+}
---
+++ Assignment 1 On Variables, Data Types/Q18. Calculate Cube of given Number.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int n = 0;
+    printf("Enter No :");
+    scanf("%d",&n);
+
+    printf("The Cube Of Given Number is %d",n*n*n);
+
+
+    getch();
+    return 0;
+}
