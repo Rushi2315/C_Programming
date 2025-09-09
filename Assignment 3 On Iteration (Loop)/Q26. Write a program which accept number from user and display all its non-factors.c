2460551326afff59288479896cbc31e334b21382
Rushi <deshmukhrushikesh51@gmail.com>
2025-09-09 10:58:16 +0530

    Add files via upload

---
+++ Assignment 3 On Iteration (Loop)/Q26. Write a program which accept number from user and display all its non-factors.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int Num = 0, i=0;
+
+    printf("Enter The Number :");
+    scanf("%d", &Num);
+
+    for(i=Num; i>=1; i--)
+    {
+        if(Num %i !=0)
+        {
+            printf("%d\n",i);
+        }
+    }
+
+    getch();
+    return 0;
+}
