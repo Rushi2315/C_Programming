eed96d34fdb9c60817f60b5232b36dd53ffb7e5a
Rushi <deshmukhrushikesh51@gmail.com>
2025-10-05 10:16:15 +0530

    Add files via upload

---
+++ Assignment 6 On Array/05 Crate Array of 7 Elements with Null (0) Initialization, Accept & Display All Elements with Loop.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int Arr[7]={0};
+    int i=0;
+
+    printf("Printing All Element By Initialization");
+
+    for(i=0; i<7; i++)
+    {
+        printf("\n Enter Element no.%d:", i+1);
+        scanf("%d", &Arr[i]);
+    }
+
+    printf("\n\n");
+
+    for(i=0; i<7; i++)
+    {
+        printf("\n Element No.%d in Array :%d",i+1,Arr[i]);
+    }
+
+    getch();
+    return 0;
+}
